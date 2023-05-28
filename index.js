f921a8481319b2cfe60284277ce46a115ddbfdf6
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-05-28 12:05:19 +0600

    Login done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const data = require('./Data/chef.json');
 const cors = require('cors');
-const port = 3000;
+const port = process.env.PORT ||3000;
 
 app.use(cors());
 
